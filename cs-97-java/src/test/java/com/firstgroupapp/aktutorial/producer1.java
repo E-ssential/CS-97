24d1b23f3611970ffda93c84a3d1857328d7ce36
henry chang <hchang23@g.ucla.edu>
2020-05-16 20:18:40 -0500

    Removed Code for Kafka Examples

--- cs-97-java/src/test/java/com/firstgroupapp/aktutorial/producer1.java
+++
@@ -1,4 +0,0 @@
-package com.firstgroupapp.aktutorial;
-
-public class producer1 {
-}
---
+++ cs-97-react/src/components/MessageForm.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class MessageForm extends React.Component{
+
+
+
+
+render(){
+    return(
+        <div className="message-form">
+            <h1>This is where a message form goes</h1>
+
+
+        </div>
+
+    )
+
+}
+
+
+
+
+
+}
