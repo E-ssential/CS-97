020740e7f61b9e4a950a7d6eb5393a09d85b3ff7
Ashley Ghodsian <ashleyarezoo@gmail.com>
2020-05-20 22:36:47 -0700

    Add information and design to About and NavBar

--- cs-97-react/src/components/About/About.js
+++ cs-97-react/src/components/About/About.js
@@ -6,11 +6,16 @@ import './About.css';
 
 const About = () =>{
     return(
-        <div className='about-page'>
-            <h1>About</h1>
-
-
-
+        <div className='outer-about'>
+            <div className='about-header'>
+                <h1>About</h1>
+            </div>
+            <div className='about-welcome'>
+                <body>Welcome to E-ssential: Your #1 stop for essential goods!</body>
+            </div>
+            <div className='about-body'>
+                <body>Please sign up or login to view current listings or create your own.</body>
+            </div>
         </div>
 
     );
