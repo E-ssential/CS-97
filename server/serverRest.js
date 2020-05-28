0e8e2f0f94f7d68b8653819b98a22b020dfbcdd5
ugpat479 <ugpat23@gmail.com>
2020-05-27 21:10:09 -0700

    updated listing backend

--- server/serverRest.js
+++ server/serverRest.js
@@ -62,75 +62,7 @@ var listings = jsonfile.readFileSync(file);
                 .send(`${JSON.stringify(newListing)}`);
   } );
 
-  // Add an endpoint to GET a list of cars
-  // it should be filterable by make with a query paramater
-  // app.get( "/addresses", 
-  //   async ( req , res  ) => {
-
-  //     console.log("get /address");
-  //     let { city } = req.query;
-
-  //     if ( !city ) {
-  //       return res.status(200)
-  //                 .send(`${JSON.stringify(adds)}`);
-  //     }
-
-  //     let address = adds.find((value  , index , obj ) => value.city == city);
-
-  //     if (! address) {
-  //       return res.status(404)
-  //                 .send(`Address in city not found`);        
-  //     }
-
-  //     return res.status(200)
-  //               .send(`Address ${JSON.stringify(address)} found for city ${city}!`);
-  // } );
-
-  // // Add an endpoint to get a specific car
-  // // it should require id
-  // // it should fail gracefully if no matching car is found
-  // app.get( "/addresses/:id", 
-  // async ( req , res  ) => {
-
-  //   console.log("get /address/:id");
-
-  //   let { id } = req.params;
-
-  //   // id can't be null, so no need to check.
-
-  //   let address  = adds.find((value , index , obj ) => value.id.toString() == id );
-
-  //   if (! address) {
-  //     return res.status(400)
-  //               .send(`Address with Id ${id} not found`);        
-  //   }
-
-  //   return res.status(200)
-  //             .send(`Address ${JSON.stringify(address)} found for Id ${id}!`);
-  // } );  
-
-  // /// Add an endpoint to post a new car to our list
-  // // it should require id, type, model, and cost
-  // app.post( "/addresses", 
-  // async ( req , res  ) => {
-
-  //   let { id, city, state, street} = req.body;
-  //   console.log(`id ${id} ${city} ${state} ${street}`);
-
-  //   if (!id || !city || !state || !street) {
-
-  //     return res.status(400)
-  //     .send(`All Address attributes id, city, state, street are required.`);        
-  //   }
-
-  //   const address   = { id:id, city:city, state:state, street:street} ;
-
-  //   adds.push(address); 
-
-  //   return res.status(200)
-  //             .send(`Address ${JSON.stringify(address)} added!`);
-  // } ); 
-
+  
   // Start the Server
   app.listen( port, () => {
       console.log( `server running http://localhost:${ port }` );
