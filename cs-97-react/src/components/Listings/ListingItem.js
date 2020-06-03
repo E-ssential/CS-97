da9a1e833640952c3bf63ad06264da7d41cbac70
Katherine <kathy022599@gmail.com>
2020-06-03 14:25:12 -0700

    updated listings page

--- cs-97-react/src/components/Listings/ListingItem.js
+++ cs-97-react/src/components/Listings/ListingItem.js
@@ -5,11 +5,40 @@ const ListingItem = ({listingData}) => {
 
 const parsedData=JSON.parse(listingData);
 return(
-    <div className='Item-Container'>
+    // <body>
+    // <div className="row">
+    //     <div className="column">
+    //         <h1>Item</h1>
+    //         <p>{parsedData.item}</p>
+    //     </div>
+    
+
+    
+    //     <div className="column">
+    //         <h1>Quantity</h1>
+    //      {/* <p>{parsedData.quantity}</p> */}
+    //     </div>
+
+    //     <div className="column">
+    //         <h1>User</h1>
+    //      {/* <p>{parsedData.quantity}</p> */}
+    //     </div>
+
+       
+    // </div>
+   
+
+       
+     <div className='Item-Container'>
+    
         <p>{parsedData.item}</p>
+       
         <p>{parsedData.quantity}</p>
         <p>{parsedData.username}</p>
-    </div>
+        </div>
+        
+   
+    // </body>
 )
 }
 export default ListingItem;
