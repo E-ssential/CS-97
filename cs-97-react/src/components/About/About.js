2a7d7e5d8dc91597d3eca21e8be416653ea49893
Iyakem A. Estifanos <iyakema.estifanos@macbook-air-3.home>
2020-06-02 22:23:21 -0700

    I have added all the files I have been working on for the last few weeks

--- cs-97-react/src/components/About/About.js
+++ cs-97-react/src/components/About/About.js
@@ -1,60 +1,53 @@
-import React from 'react';
-import './About.css';
-import Logo from '../../icons/E-ssentialLogo.png'
-import BackgroundImg from '../../icons/About.png'
+import React from "react";
+import "./About.css";
+import Logo from "../../icons/oklogo.png";
+import BackgroundImg from "../../icons/About.png";
 
+const About = ({ isAuth, userData }) => {
+  const userID = userData[0];
+  const username = userData[1];
+  const email = userData[2];
 
-
-const About = ({isAuth, userData}) =>{
-    const userID = userData[0];
-    const username = userData[1];
-    const email = userData[2];
-
-    return(
-       isAuth?
-
-        (
-       
-        <div className='outer-about'>
-            <div className="back">
-            <div className='about-header'>
-                <img src={Logo} alt="Essential-logo" width="350" className="EssentialLogo"/>
-                <h1>About</h1>
-            </div>
-            <div className='about-welcome'>
-                <h3>Welcome back {username}! Hope you are having a good day</h3>
-            </div>
-            </div>
+  return isAuth ? (
+    <div className="outer-about">
+      <div className="back">
+        <div className="about-header">
+          <img
+            src={Logo}
+            alt="Essential-logo"
+            width="500"
+            height="400"
+            className="EssentialLogo"
+          />
         </div>
-
-        )
-
-        :
-
-        (
-
-        <div className='outer-about'>
-            <div className="back">
-            <div className='about-header'>
-                <img src={Logo} alt="Essential-logo" width="350" className="EssentialLogo"/>
-                <h1>About</h1>
-            </div>
-            <div className='about-welcome'>
-                <h3>Welcome to E-ssential: Your #1 stop for essential goods!</h3>
-            </div>
-            <div className='about-body'>
-                <h3>Please sign up or login to view current listings or create your own.</h3>
-            </div>
-            
-            </div>
+        <div className="about-welcome">
+          <h1>Welcome back {username}! Hope you are having a good day</h1>
         </div>
-
-
-            
-        )
-
-    )
-
-}
+      </div>
+    </div>
+  ) : (
+    <div className="outer-about">
+      <div className="back">
+        <div className="about-header">
+          <img
+            src={Logo}
+            alt="Essential-logo"
+            width="500"
+            height="400"
+            className="EssentialLogo"
+          />
+        </div>
+        <div className="about-welcome">
+          <h3>Welcome to E-ssential: Your #1 stop for essential goods!</h3>
+        </div>
+        <div className="about-body">
+          <h3>
+            Please sign up or login to view current listings or create your own.
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default About;
--- cs-97-react/src/components/NavBar/NavBar.js
+++ cs-97-react/src/components/NavBar/NavBar.js
@@ -1,92 +1,82 @@
-import React, {useEffect} from 'react';
-import { Link } from 'react-router-dom';
-import './NavBar.css';
-import axios from 'axios';
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import Logo from "../../icons/oklogo.png";
+import "./NavBar.css";
+import axios from "axios";
 
+const NavBar = ({ isAuth, username, setAuth, checkLogin }) => {
+  const logOut = () => {
+    axios
+      .get("http://localhost:5000/users/logout", { withCredentials: true })
+      .then((res) => {
+        setAuth(false);
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  };
 
-const NavBar = ({isAuth, username, setAuth, checkLogin}) => {
-    
-
-    const logOut = () =>{
-        axios.get('http://localhost:5000/users/logout', {withCredentials:true})
-        .then(res => {
-            setAuth(false);
-        })
-        .catch(err => {
-            alert(err);
-            
-        })
-        
+  useEffect(() => {
+    if (!username) {
+      checkLogin();
     }
+  }, []);
 
-    useEffect( () => {
-        if(!username){
-            checkLogin();
-        }
-    }, [])
+  return isAuth ? (
+    // IS AUTHORIZED
+    <div className="nav-bar">
+      <div className="nav-left">
+        <Link to="/">
+          <img
+            src={Logo}
+            alt="Essential-logo"
+            width="90"
+            height="75"
+            className="EssentialLogo"
+          />
+        </Link>
+      </div>
 
-    return(
-
-        isAuth ?
-
-        // IS AUTHORIZED
-        (
-            <div className='nav-bar'>
-            <div className='nav-left'>
-                <Link to='/' >
-                        <button className="nav-button" >Home Page</button>
-                </Link>
-
-                <Link to='/listingsForm' >
-                        <button className="nav-button" >Listing Form</button>
-                </Link>
-                <Link to='/listingspage' >
-                        <button className="nav-button" >View Items</button>
-                </Link>
-                <Link to='/joinRoom' >
-                        <button className="nav-button" >Chat</button>
-                </Link>
-            </div>
-
-            <div className='nav-right'>
-                <Link to='/profile'>
-                    <button className='nav-button'>Profile</button>
-                </Link>
-                <Link to='/' >
-                        <button className="nav-button" onClick={logOut}>Log Out</button>
-                </Link>
-
-            </div>
-        </div>
-        
-    )
-
-    :
-
+      <div className="nav-right">
+        <Link to="/listingsForm">
+          <button className="nav-button">Listing Form</button>
+        </Link>
+        <Link to="/listingspage">
+          <button className="nav-button">View Items</button>
+        </Link>
+        <Link to="/joinRoom">
+          <button className="nav-button">Chat</button>
+        </Link>
+        <Link to="/profile">
+          <button className="nav-button">Profile</button>
+        </Link>
+        <Link to="/">
+          <button className="nav-button" onClick={logOut}>
+            Log Out
+          </button>
+        </Link>
+      </div>
+    </div>
+  ) : (
     // NOT AUTHORIZED
-    (
-        <div className='nav-bar'>
-            <div className='nav-left'>
-                <Link to='/' >
-                        <button className="nav-button" >Home Page</button>
-                </Link>
+    <div className="nav-bar">
+      <div className="nav-left">
+        <Link to="/">
+          <button className="nav-button">Home Page</button>
+        </Link>
 
-                <Link to='/listingspage' >
-                        <button className="nav-button" >View Items</button>
-                </Link>
-            </div>
+        <Link to="/listingspage">
+          <button className="nav-button">View Items</button>
+        </Link>
+      </div>
 
-            <div className='nav-right'>
-
-                <Link to='/login' >
-                        <button className="nav-button" >Login</button>
-                </Link>
-
-            </div>
-        </div>
-        
-    )
-    )
-}
+      <div className="nav-right">
+        <Link to="/login">
+          <button className="nav-button">Login</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default NavBar;
