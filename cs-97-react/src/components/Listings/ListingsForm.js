afa7bf37e358de7fa0eee3b8b888c5355a2a3433
Iyakem A. Estifanos <iyakema.estifanos@macbook-air-3.home>
2020-06-02 21:56:00 -0700

    This is the good font

--- cs-97-react/src/components/Listings/ListingsForm.js
+++ cs-97-react/src/components/Listings/ListingsForm.js
@@ -1,93 +1,82 @@
-import React, { useState, useEffect }from 'react';
-import './ListingForm.css';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import "./ListingForm.css";
+import axios from "axios";
 
-const ListingsForm = ({isAuth, userData}) => {
+const ListingsForm = ({ isAuth, userData }) => {
+  const [user, setUser] = useState(userData[1]);
+  const [item, setItem] = useState("");
+  const [quantity, setQuantity] = useState(0);
+  const [address, setAddress] = useState("");
+  const [status, setStatus] = useState("");
 
-    const [user, setUser] = useState(userData[1]);
-    const [item, setItem] = useState('');
-    const [quantity, setQuantity] = useState(0);
-    const [address, setAddress] = useState('');
-    const [status, setStatus] = useState('');
+  const onSubmit = (e) => {
+    e.preventDefault();
 
-    const onSubmit = (e) => {   
-      e.preventDefault();
+    const requestOptions = { user, item, quantity, address };
 
-        const requestOptions = 
-        {user, 
-        item, 
-        quantity, 
-        address};
-        
-        axios.post('http://localhost:5000/listings/add', requestOptions, {withCredentials:true})
-              .then(res => {
-                  setStatus(res.data);
-              })
-              .catch(err => {
-                console.log(err.response);
-                setStatus(err.response.data);
-        })
-        
-      
-      
-    }
-    
-      return (
+    axios
+      .post("http://localhost:5000/listings/add", requestOptions, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        setStatus(res.data);
+      })
+      .catch((err) => {
+        console.log(err.response);
+        setStatus(err.response.data);
+      });
+  };
 
-        <div className='outer-listing'>
-          
-          <div className='listing-heading'>
-              <h1> Listing Form</h1>
-          <div className='listingBut'> 
+  return (
+    <div className="outer-listing">
+      <div className="listing-heading">
+        <h1> Listing Form</h1>
+        <div className="listingBut">
           {/* <form onSubmit={this.handleSubmit}> */}
-         
-           
 
+          <form onSubmit={onSubmit}>
+            <p>{status}</p>
+            <div className="Item">
+              <label>
+                Item
+                <input
+                  type="text"
+                  onChange={(event) => setItem(event.target.value)}
+                  required
+                />
+              </label>
+            </div>
 
-         <form onSubmit={onSubmit}>
+            <div className="Quantity">
+              <label>
+                Quantity
+                <input
+                  type="number"
+                  min="0"
+                  step="1"
+                  onChange={(event) => setQuantity(event.target.value)}
+                  required
+                />
+              </label>
+            </div>
 
-          <p>{status}</p>
-          <div>     
-          <label>
-            Item      
-            <input type="text"  onChange={(event) => setItem(event.target.value)} required />
-          </label>
-          
-          </div>     
+            <div className="listings-info">
+              <label>
+                Address
+                <input
+                  type="text"
+                  onChange={(event) => setAddress(event.target.value)}
+                  required
+                />
+              </label>
+            </div>
 
-          <div >     
-          <label>
-            Quantity      
-            <input type="number"  min="0" step="1" onChange={(event) => setQuantity(event.target.value)} required />
-          </label>
-          
-          </div>     
-
-          <div className="listings-info">     
-           <label>
-            Address     
-            <input type="text"  onChange={(event) => setAddress(event.target.value)} required />
-           </label>
-          
-          </div>     
-          
-          <button type="submit">Submit
-         
-          </button>
-         
-         
+            <button type="submit">Submit</button>
           </form>
-          
-          
-         
-          </div>
-          </div>
-         
-
         </div>
+      </div>
+    </div>
+  );
+};
 
-      );
-    
-  }
-  
 export default ListingsForm;
--- cs-97-react/src/components/Listings/ViewListings.js
+++ cs-97-react/src/components/Listings/ViewListings.js
@@ -1,70 +1,48 @@
-import React, {useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
+import ListingSearch from "./ListingSearch.js";
+import ListingList from "./ListingList";
+import "./ViewListings.css";
 
-import ListingSearch from './ListingSearch.js';
-import ListingList from './ListingList';
-import './ViewListings.css'
+const ListingsPage = ({ isAuth, userData }) => {
+  const [listings, setListings] = useState([]);
+  const [isFetch, setFetch] = useState(true);
+  var timer;
+  //START HERE TOMORROW
 
+  const fetchListings = async (request) => {
+    await axios
+      .post("http://localhost:5000/listings/searchItems", request, {
+        withCredentials: true,
+      })
+      .then(async (res) => {
+        await setListings(res.data);
+        await setFetch(false);
+      })
+      .catch(async (err) => {
+        await setFetch(false);
+      });
+  };
 
-const ListingsPage = ({isAuth, userData}) => {
-    const [listings, setListings] = useState([]);
-    const [isFetch, setFetch] = useState(true);
-    var timer;
-    //START HERE TOMORROW
+  useEffect(() => {
+    fetchListings();
+  }, [isFetch]);
 
-        const fetchListings = async (request) =>{
-            
-            await axios.post('http://localhost:5000/listings/searchItems', request, {withCredentials:true})
-            .then(async res =>{
-                await setListings(res.data);
-                await setFetch(false);
-
-            })
-            .catch(async err => {
-                await setFetch(false);
-            });
- 
-        }
-        
-        useEffect( () => {
-            
-            fetchListings();
-        }, [isFetch])
-
-
-
-        return(
-
-            isFetch ?
-
-            (
-                <div className="viewListings">
-                    <p>'Please wait, Fetching Data...'</p>
-                 </div>
-            )
-
-            :
-
-            (
-
-            <div className="viewListings">
-                <ListingSearch fetchListings={fetchListings}/>
-                <ListingList allListings={listings}/>
-                
-            </div>
-
-
-            )
-        )
-
-
-}
-
-
-
-
-
-
+  return isFetch ? (
+    <div className="viewListings">
+      <p>'Please wait, Fetching Data...'</p>
+    </div>
+  ) : (
+    <div className="Listbox">
+      <div className="searcher">
+        <ListingSearch fetchListings={fetchListings} />
+      </div>
+      <div className="theListings">
+        <ListingList allListings={listings} />
+      </div>
+    </div>
+  );
+};
 
 export default ListingsPage;
