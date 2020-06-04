12b064d70e9de3003bf897d3638c68a3345ea8c4
ugpat479 <ugpat23@gmail.com>
2020-06-03 18:26:30 -0700

    redirect users to joinRoom, create a chat room for specific listing

--- cs-97-react/src/components/Listings/ListingItem.js
+++ cs-97-react/src/components/Listings/ListingItem.js
@@ -1,77 +1,60 @@
-
+import axios from "axios";
 import React, {useState, useEffect } from 'react';
-import './ListingItem.css';
+import './ViewListings.css';
+
+function goToChat(listing){
+
+    var room = listing.item+listing.username; 
+    const data = {
+        newRoom: room,
+    };
+    
+    axios.post("http://localhost:5000/users/addRoom", data, {
+        withCredentials: true,
+        });
+      
+    window.location.replace("http://localhost:3000/selectRoom");
+}
 
 const ListingItem = ({listingData}) => {
 
 const parsedData=JSON.parse(listingData);
 return(
-  
+    // <body>
+    // <div className="row">
+    //     <div className="column">
+    //         <h1>Item</h1>
+    //         <p>{parsedData.item}</p>
+    //     </div>
     
+
+    
+    //     <div className="column">
+    //         <h1>Quantity</h1>
+    //      {/* <p>{parsedData.quantity}</p> */}
+    //     </div>
+
+    //     <div className="column">
+    //         <h1>User</h1>
+    //      {/* <p>{parsedData.quantity}</p> */}
+    //     </div>
+
+       
+    // </div>
    
 
        
-   
-  
-        /* <div className="Item-Container">
-                
-                
-            <div className="item">
-                <h2>Item</h2>
-                
-            </div>
-
-
-            <div className="item">
-                <h2>Quantity</h2> 
-                
-            </div>
-
-            <div className="item">
-                <h2>User</h2>
-            
-            </div>
-
-
-
-        </div> */
-        
-        <div className="Item-Container">
-            
-            
-                <div className="item">
-                    {/* <h2>Item</h2> */}
-                    <p>{parsedData.item}</p>
-                </div>
-        
-            
-
-
-           
-                <div className="item">
-                    {/* <h2>Quantity</h2>  */}
-                     <p>{parsedData.quantity}</p>
-                </div>
-        
-            
-
-
-         
-                <div className="item">
-                    {/* <h2>User</h2> */}
-                    <p>{parsedData.username}</p>
-                </div>
-        
-           
-
-        </div>
+     <div className='Item-Container'>
     
-
-
-        
+        <p>{parsedData.item}</p>
+       
+        <p>{parsedData.quantity}</p>
+        <p>{parsedData.username}</p>
+        <button onClick={() => { goToChat(parsedData) }}>chat with {parsedData.username}</button>
+        </div>
         
    
-  
+    // </body>
 )
 }
 export default ListingItem;
